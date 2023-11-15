9633387f2d51bc5f9bf7658d18374c25cd34d7b7
olicoding <oliveira.sein@gmail.com>
2023-11-15 23:57:29 +0100

    Testing pipeline with Jest and linter

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  rootDir: "./",
+  modulePathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "<rootDir>/tests/styleMock.js",
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  setupFiles: ["./tests/environment.js"],
+  setupFilesAfterEnv: ["./tests/setup.js"],
+  collectCoverageFrom: ["components/**/*.{js,jsx}", "pages/**/*.{js,jsx}"],
+  coverageReporters: ["lcov", "text", "text-summary"],
+  transform: {
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+};
---
+++ tests/setup.js
@@ -0,0 +1,21 @@
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(() => {
+  jest.clearAllMocks();
+  jest.restoreAllMocks();
+  jest.resetModules();
+});
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    asPath: "/",
+  }),
+}));
+
+jest.mock("@auth0/nextjs-auth0", () => {
+  return {
+    getAccessToken: () => "access_token",
+    withApiAuthRequired: (handler) => handler,
+    withPageAuthRequired: (page) => () => page(),
+  };
+});
