857805141edd17e688730325f770ed448bb1dc1a
olicoding <oliveira.sein@gmail.com>
2023-11-15 03:40:06 +0100

    ESLint warnings fix

--- pages/api/shows.js
+++ pages/api/shows.js
@@ -1,19 +1,19 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function shows(req, res) {
   try {
     const { accessToken } = await getAccessToken(req, res, {
-      scopes: ['read:shows']
+      scopes: ["read:shows"],
     });
     const apiPort = process.env.API_PORT || 3001;
     const response = await fetch(`http://localhost:${apiPort}/api/shows`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
-    const shows = await response.json();
+    const apiShows = await response.json();
 
-    res.status(200).json(shows);
+    res.status(200).json(apiShows);
   } catch (error) {
     res.status(error.status || 500).json({ error: error.message });
   }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,14 @@
 export default function Home() {
   return (
-    <>
-      <div className="home-page">
-        <main className="main">
-          <div className="bg-image">
-            <section className="content">
-              <h1 className="title">Coming Soon!</h1>
-              <p className="description">New project under construction.</p>
-            </section>
-          </div>
-        </main>
-      </div>
-    </>
+    <div className="home-page">
+      <main className="main">
+        <div className="bg-image">
+          <section className="content">
+            <h1 className="title">Coming Soon!</h1>
+            <p className="description">New project under construction.</p>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
