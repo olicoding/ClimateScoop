bf5b6aa750874ad51885d4f756929620cc5f9c27
olicoding <oliveira.sein@gmail.com>
2023-11-16 17:30:22 +0100

    Cypress config adjustments

--- cypress.config.js
+++ cypress.config.js
@@ -1,37 +1,24 @@
 const { defineConfig } = require("cypress");
 
-module.exports = (on, config) => {
-  const cypressConfig = defineConfig({
-    chromeWebSecurity: false,
-    viewportWidth: 1000,
-    viewportHeight: 1000,
-    reporter: "junit",
-    video: true,
-    videoUploadOnPasses: false,
-    videosFolder: "cypress/videos",
-    fixturesFolder: "cypress/fixtures",
-    screenshotsFolder: "cypress/screenshots",
-    reporterOptions: {
-      mochaFile: "test-results/cypress/junit-[hash].xml",
-    },
-    retries: {
-      runMode: 3,
-    },
-    e2e: {
-      setupNodeEvents(on, config) {},
-      baseUrl: "http://localhost:3000",
-      specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    },
-  });
-
-  on("before:browser:launch", (browser, launchOptions) => {
-    if (browser.name === "chrome") {
-      launchOptions.args.push("--disable-dev-shm-usage");
-    }
-    return launchOptions;
-  });
-
-  config.baseUrl = process.env.CYPRESS_BASE_URL || "http://localhost:3000";
-
-  return Object.assign({}, cypressConfig, config);
-};
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1000,
+  viewportHeight: 1000,
+  reporter: "junit",
+  video: true,
+  videoUploadOnPasses: false,
+  videosFolder: "cypress/videos",
+  fixturesFolder: "cypress/fixtures",
+  screenshotsFolder: "cypress/screenshots",
+  reporterOptions: {
+    mochaFile: "test-results/cypress/junit-[hash].xml",
+  },
+  retries: {
+    runMode: 3,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+});
