c84212e473bdfcf05f64a73d6d88a1305c167c66
olicoding <oliveira.sein@gmail.com>
2023-11-16 16:04:35 +0100

    Integrating Cypress with config files and updating CI pipeline

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1000,
+  viewportHeight: 1000,
+  reporter: "junit",
+  video: true,
+  videoUploadOnPasses: false,
+  videosFolder: "cypress/videos",
+  fixturesFolder: "cypress/fixtures",
+  screenshotsFolder: "cypress/screenshots",
+  reporterOptions: {
+    mochaFile: "test-results/cypress/junit-[hash].xml",
+  },
+  retries: {
+    runMode: 3,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+});
