a43ded81c96643262ae8b5bbd4b9998d0529b793
olicoding <oliveira.sein@gmail.com>
2023-11-21 04:36:39 +0100

    General improvements and redeploy with config changes

--- context/ContextProvider.jsx
+++ context/ContextProvider.jsx
@@ -1,9 +1,32 @@
-import { createContext, useRef } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { createContext, useRef, useMemo } from "react";
 
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
+  const { user, isLoading } = useUser();
   const chartsRef = useRef(null);
 
-  return <Context.Provider value={chartsRef}>{children}</Context.Provider>;
+  const username = useMemo(() => {
+    if (isLoading) return null;
+    if (!user) return "Guest";
+
+    return user["https://climatescoop/username"]
+      ? `${user["https://climatescoop/username"].charAt(0).toUpperCase()}${user[
+          "https://climatescoop/username"
+        ].slice(1)}`
+      : user.nickname || "Guest";
+  }, [user, isLoading]);
+
+  const contextValue = useMemo(
+    () => ({
+      chartsRef,
+      user,
+      username,
+      isLoading,
+    }),
+    [chartsRef, user, username, isLoading]
+  );
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 };
