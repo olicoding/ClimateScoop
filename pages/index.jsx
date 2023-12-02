2ae6ba80b80849fb461ac33d15a845718e53e651
olicoding <oliveira.sein@gmail.com>
2023-12-02 19:52:46 +0100

    Integrated renewable energy bar chart.

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,38 +9,60 @@ export default function Home(props) {
 
 export async function getStaticProps() {
   try {
-    const apiEndpoints = [
-      "https://global-warming.org/api/temperature-api",
-      "https://global-warming.org/api/ocean-warming-api",
-      "https://global-warming.org/api/arctic-api",
+    const apiDataMappings = [
+      {
+        url: "https://global-warming.org/api/temperature-api",
+        key: "globalData",
+        dataPath: "result",
+      },
+      {
+        url: "https://global-warming.org/api/ocean-warming-api",
+        key: "oceanData",
+        dataPath: "result",
+      },
+      {
+        url: "https://global-warming.org/api/arctic-api",
+        key: "arcticData",
+        dataPath: "arcticData",
+      },
+      {
+        url: `${process.env.ENV_DOMAIN}/api/data`,
+        key: "energyData",
+        dataPath: null,
+      },
     ];
 
-    const fetchApiData = (url) => axios.get(url).then((res) => res.data);
+    const fetchApiData = (url) =>
+      axios.get(url, { timeout: 5000 }).then((res) => res.data);
 
-    const fetchAndCacheData = async (key, url) => {
-      try {
-        return await getCachedData(key, () => fetchApiData(url));
-      } catch (error) {
-        logger.error(`Failed to fetch data for ${key}: ${error.message}`);
-        return null;
+    const fetchAndCacheData = async (key, url) =>
+      getCachedData(key, () => fetchApiData(url), 30 * 24 * 60 * 60);
+
+    const apiDataPromises = apiDataMappings.map(({ url, key }) =>
+      fetchAndCacheData(key, url)
+    );
+
+    const results = await Promise.allSettled(apiDataPromises);
+
+    const props = apiDataMappings.reduce((acc, { key, dataPath }, index) => {
+      const result = results[index];
+      if (result.status === "fulfilled") {
+        acc[key] = dataPath
+          ? result.value?.[dataPath] || null
+          : result.value || null;
+      } else {
+        acc[key] = null;
+        logger.error(`Failed to fetch data for ${key}: ${result.reason}`);
       }
-    };
-
-    const [globalData, oceanData, arcticData] = await Promise.all([
-      fetchAndCacheData("globalData", apiEndpoints[0]),
-      fetchAndCacheData("oceanData", apiEndpoints[1]),
-      fetchAndCacheData("arcticData", apiEndpoints[2]),
-    ]);
+      return acc;
+    }, {});
 
     return {
-      props: {
-        globalData: globalData ? globalData.result : null,
-        oceanData: oceanData ? oceanData.result : null,
-        arcticData: arcticData ? arcticData.arcticData : null,
-      },
+      props,
       revalidate: 86400,
     };
   } catch (error) {
+    logger.error(`Error in getStaticProps: ${error.message}`);
     return {
       props: { chartsData: null },
     };
