ff39654e50247e900b2048722e59600a4b1b24d9
olicoding <oliveira.sein@gmail.com>
2023-12-10 00:25:59 +0100

    Updated privacy terms.

--- tests/components/PrivacyBanner.test.jsx
+++ tests/components/PrivacyBanner.test.jsx
@@ -4,13 +4,17 @@ import PrivacyBanner from "../../components/PrivacyBanner";
 describe("PrivacyBanner", () => {
   it("renders the privacy banner message", () => {
     render(<PrivacyBanner />);
-    expect(screen.getByText(/Your privacy matters/)).toBeInTheDocument();
+    expect(
+      screen.getByText(/authentication and basic analytics/)
+    ).toBeInTheDocument();
   });
 
   it("disappears after clicking the acknowledge button", () => {
     render(<PrivacyBanner />);
     const acknowledgeButton = screen.getByText("okay!");
     fireEvent.click(acknowledgeButton);
-    expect(screen.queryByText(/Your privacy matters/)).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/authentication and basic analytics/)
+    ).not.toBeInTheDocument();
   });
 });
